15038ca7a2d05e2a22a3532026c6b757d567ec6b
BayLadyCoder <rachada_32@hotmail.com>
2021-04-07 19:49:13 -0400

    Removed old container file

--- src/components/DisplayTodoList/DisplayTodos.container.js
+++
@@ -1,9 +0,0 @@
-import { connect } from "react-redux";
-import DisplayTodos from "./DisplayTodoList";
-import { reorderTodos } from "../../redux/todoActions";
-
-const mapStateToProps = (state) => ({
-  todos: state.todos,
-});
-
-export default connect(mapStateToProps, { reorderTodos })(DisplayTodos);
