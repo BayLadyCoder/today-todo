9363dc27a6423567276a5a4db7433ebda9c716a6
BayLadyCoder <rachada_32@hotmail.com>
2021-04-08 23:42:11 -0400

    Updated todoActions to use TypeScript

--- src/redux/todoActions.ts
+++ src/redux/todoActions.ts
@@ -1,3 +1,12 @@
+import { TodoType } from "../Types/Types";
+import {
+  AddTodo,
+  UpdateTodo,
+  DeleteTodo,
+  CompleteTodo,
+  ReorderTodoList,
+} from "../Types/ActionTypes";
+
 // Action Types
 export const ADD_TODO = "ADD_TODO";
 export const UPDATE_TODO = "UPDATE_TODO";
@@ -5,19 +14,19 @@ export const DELETE_TODO = "DELETE_TODO";
 export const COMPLETE_TODO = "COMPLETE_TODO";
 export const REORDER_TODO_LIST = "REORDER_TODO_LIST";
 
-export const addTodo = (todo) => {
+export const addTodo = (todo: TodoType): AddTodo => {
   return {
     type: ADD_TODO,
     payload: todo,
   };
 };
-export const updateTodo = (id, data) => {
+export const updateTodo = (id: string, data: string): UpdateTodo => {
   return {
     type: UPDATE_TODO,
     payload: { id, data },
   };
 };
-export const deleteTodo = (id) => {
+export const deleteTodo = (id: string): DeleteTodo => {
   return {
     type: DELETE_TODO,
     payload: {
@@ -25,13 +34,13 @@ export const deleteTodo = (id) => {
     },
   };
 };
-export const completeTodo = (id) => {
+export const completeTodo = (id: string): CompleteTodo => {
   return {
     type: COMPLETE_TODO,
     payload: { id },
   };
 };
-export const reorderTodoList = (todoList) => {
+export const reorderTodoList = (todoList: TodoType[]): ReorderTodoList => {
   return {
     type: REORDER_TODO_LIST,
     payload: todoList,
